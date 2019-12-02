45cd430565501b8e8813cf303b5f62bad8579a6c
Pete Anderson <me@bipvanwinkle.com>
2019-12-01 19:58:01 -0700

    Making iron ore it's own food type

--- modmain.lua
+++ modmain.lua
@@ -3,6 +3,9 @@ local assets=
 	Asset("ANIM", "anim/iron_ore.zip"),
 }
 
+-- We are going to put setup code here until we figure out a better way to organize things
+FOODTYPE["IRON_ORE"] = "IRON_ORE"
+
 local function onsave(inst, data)
 	data.anim = inst.animname
 end
@@ -47,16 +50,15 @@ local function fn()
 	--inst.components.repairer.repairmaterial = "orp"
 	--inst.components.repairer.healthrepairvalue = TUNING.REPAIR_ROCKS_HEALTH
     
-    -- In the future I'll make it edible with it's own type
-    --inst:AddComponent("edible")
-    --inst.components.edible.foodtype = FOODTYPE.IRON_ORE
-    --inst.components.edible.healthvalue = TUNING.HEALING_HUGE
-    --inst.components.edible.hungervalue = TUNING.CALORIES_HUGE
-    --inst.components.edible.sanityvalue = TUNING.SANITY_HUGE
+    inst:AddComponent("edible")
+    inst.components.edible.foodtype = FOODTYPE.IRON_ORE
+    inst.components.edible.healthvalue = TUNING.HEALING_HUGE
+    inst.components.edible.hungervalue = TUNING.CALORIES_HUGE
+    inst.components.edible.sanityvalue = TUNING.SANITY_HUGE
 
     inst.OnSave = onsave 
     inst.OnLoad = onload 
     return inst
 end
 
-return Prefab( "common/inventory/iron", fn, assets) 
+return Prefab( "iron", fn, assets) 
